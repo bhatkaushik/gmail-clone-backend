0754d53b1b3b4f4b9fdbea6bc037150915505663
Kaushik G Bhat <91201263+bhatkaushik@users.noreply.github.com>
2022-11-20 20:00:58 +0530

    Update email-schema.js

--- schema/email-schema.js
+++ schema/email-schema.js
@@ -1,12 +1,11 @@
 import mongoose from "mongoose";
-import autoIncrement from 'mongoose-auto-increment'
+
 const emailDataSchema = mongoose.Schema({
     To:String,
     Subject: String,
     Message: String
 })
-autoIncrement.initialize(mongoose.connection)
-emailDataSchema.plugin(autoIncrement.plugin,'Emaildata')
+
 
 const Emaildata= mongoose.model('emaildata',emailDataSchema);
 
